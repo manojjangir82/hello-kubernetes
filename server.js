b3ddf0448922e95b916b86ad0e628f328b95de87
manojjangir82 <c.manoj.k@gmail.com>
2019-07-23 18:30:02 +0530

    Second pUsh

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(morgan('combined'));
 
 // Configuration
 var port = process.env.PORT || 8080;
-var message = process.env.MESSAGE || "Welcome to CI/CD Demo!";
+var message = process.env.MESSAGE || "Hello world!";
 
 app.get('/', function (req, res) {
     res.render('home', {
